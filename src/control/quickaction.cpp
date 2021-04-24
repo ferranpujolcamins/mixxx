2814044172fa4b3f32d30383baa78990ddfeb365
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2021-04-24 10:39:38 +0200

    temp

--- src/control/quickaction.cpp
+++ src/control/quickaction.cpp
@@ -1,31 +1,64 @@
 #include "quickaction.h"
 
+#include "control/controlproxy.h"
 #include "preferences/usersettings.h"
 
 QuickAction::QuickAction(QObject* parent)
         : QObject(parent),
-          m_recordingMacro("[QuickAction]", "recording"),
-          m_trigger("[QuickAction]", "trigger") {
-    m_trigger.connectValueChanged(this, &QuickAction::trigger);
+          m_coRecordingMacro(ConfigKey("[QuickAction]", "recording")),
+          m_coTrigger(ConfigKey("[QuickAction]", "trigger")),
+          m_recordedValues(100),
+          m_recordingSession(0) {
 }
 
 bool QuickAction::recordCOValue(const ConfigKey& key, double value) {
-    if (m_recordingMacro.toBool()) {
-        m_recordedValues.emplace_back(key, value);
-        m_validPosition[key] = m_recordedValues.size() - 1;
-        return true;
+    RecordingSessionId recordingSession =
+            m_recordingSession.load() if (recordingSession.recording) {
+        // TODO: what happens if the sessions ends before we emplace?
+        // TODO: we immediately trigger right?
+        // TODO: how to gracefully handle the case where the queue is full?
+        if (m_recordedValues.try_emplace(key, value)) {
+            if (m_recordingSession.load() != recordingSession) {
+                return false;
+            }
+        }
+        //
     }
     return false;
 }
 
 void QuickAction::trigger(double) {
-    for (unsigned int i = 0; i < m_recordedValues.size(); ++i) {
-        ConfigKey key = m_recordedValues[i].first;
-        double value = m_recordedValues[i].second;
-        if (m_validPosition[key] == i) {
-            ControlProxy(key).set(value);
+    m_recordingBlocked.store(true);
+    m_recordingSession.stop();
+
+    QHash<ConfigKey, double> values;
+    QueueElement recordedValue;
+    // We need to make room for the placeholder, since the queue could be full.
+    while (!m_recordedValues.try_emplace()) {
+        // Since try_emplace failed, we know there's something on the queue, thus pop won't block.
+        // TODO: can this invariant break here? because someone else pops maybe?
+
+        m_recordedValues.pop(recordedValue);
+        // TODO: can the poped value be from the wrong recording session?
+        values[recordedValue.m_key] = recordedValue.m_dValue;
+    }
+
+    // TODO: could someone else change the recording session value while doing all this ^ ???
+    // TODO: What happens if elements are added to the queue while doing this??
+
+    m_recordingSession.increase();
+
+    QHash<ConfigKey, double> values;
+
+    while (m_recordedValues.try_pop(recordedValue) &&
+            !recordedValue.m_bEndOfRecordingSessionPlaceholder) {
+        if (recordedValue.m_recordingSessionId == recordingSessionId) {
+            values[recordedValue.m_key] = recordedValue.m_dValue;
         }
     }
-    m_recordedValues.clear();
-    m_validPosition.clear();
+
+    for (ConfigKey configKey : values.keys()) {
+        double value = values[configKey];
+        ControlProxy(configKey).set(value);
+    }
 }
--- src/control/quickaction.h
+++ src/control/quickaction.h
@@ -1,43 +1,89 @@
 #pragma once
 
 #include <QObject>
-#include <utility>
 
-#include "control/controlproxy.h"
+#include "control/controlobject.h"
+#include "rigtorp/MPMCQueue.h"
 
 class ConfigKey;
+
+// TODO: memory order & memory barriers?
 
 class QuickAction : public QObject {
     Q_OBJECT
   public:
     explicit QuickAction(QObject* parent = nullptr);
+
     // Record a new value for a CO.
+    //
+    // You must pass to the recordingSession parameter the value you read from .... XXXX
+    //
     // Returns true if the macro recorder was in recording state and the new value was recorded,
     // returns false if the new value was not recorded.
     bool recordCOValue(const ConfigKey& key, double value);
 
     void trigger(double value);
 
-  private:
-    ControlProxy m_recordingMacro;
-    ControlProxy m_trigger;
+    // A positive value means we are recording. A negative value indicates we are not recording.
+    // The magnitude of the value is used to determine what "recording session" a control change belongs to.
+    //
+    // COs read this value to determine whether they should call the recordCOValue method (positive value) or not (negative value).
+    // When COs call the recordCOValue method, they must pass back the recordingSession value they got. This allows us to detect CO values
+    // that we receive after the QuickAction has already been triggered.
+    RecordingSessionId recordingSession() {
+        return RecordingSessionId(m_recordingSession.load());
+    }
 
-    class Key {
+  private:
+    using RecordingSessionId = unsigned int;
+
+    void toggleRecording() {
+        m_recordingSession += QuickAction::recordingBoundary;
+    }
+
+    // Ends recording session and increases m_recordingSession so its ready for next session.
+    // Returns the id of the just ended session
+    unsigned int endRecordingSession() {
+        // Add placeholder to queue
+        m_recordedValues.try_emplace();
+        // TODO: what if a recording is inserted after the placeholder but before the recording session is increased??
+
+        // TODO: can we handle gracefully the case when the queue is full?
+        // If we could get the nª of elements in the queue we could count how many we pop so we wouldn't need the placeholder.
+        // Or modify MPMC queue to have a better api to consume the queue up to the current last element
+
+        return m_recordingSession.fetch_add(std::numeric_limits<unsigned int>::digits);
+    }
+
+    ControlObject m_coRecordingMacro;
+    ControlObject m_coTrigger;
+
+    // If m_recordingSession >= boundary, it means the quick action is recording. Otherwise, the quick action is not recording.
+    static constexpr int recordingBoundary = std::numeric_limits<unsigned int>::digits - 1;
+    std::atomic_uint m_recordingSession;
+    static_assert(std::numeric_limits<unsigned int>::is_modulo);
+    static_assert(std::numeric_limits<unsigned int>::radix == 2);
+
+    std::atomic_bool m_recordingBlocked;
+
+    class QueueElement {
       public:
-        Key(ConfigKey configKey, int ordinal)
-                : m_configKey(std::move(configKey)), m_iOrdinal(ordinal) {
+        QueueElement() noexcept
+                : m_bEndOfRecordingSessionPlaceholder(true) {
         }
 
-        // We use configKey to determine whether two keys are equal or not, thus only one Key with a given configKey can
-        // exist in the QMap.
-        ConfigKey m_configKey;
+        QueueElement(ConfigKey key, double dValue, RecordingSessionId recordingSessionId) noexcept
+                : m_key(key),
+                  m_dValue(dValue),
+                  m_bEndOfRecordingSessionPlaceholder(false),
+                  m_recordingSessionId(recordingSessionId) {
+        }
 
-        // We use ordinal to sort the keys, so the recorded CO are triggered in the same order they were recorded.
-        int m_iOrdinal;
-
-        bool operator<(const QuickAction::Key& other) const;
+        ConfigKey m_key;
+        double m_dValue;
+        bool m_bEndOfRecordingSessionPlaceholder;
+        RecordingSessionId m_recordingSessionId;
     };
 
-    std::vector<std::pair<ConfigKey, double>> m_recordedValues;
-    QHash<ConfigKey, unsigned int> m_validPosition;
+    rigtorp::MPMCQueue<QueueElement> m_recordedValues;
 };
